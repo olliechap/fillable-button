cb6269a5a717838de94779a7b0cbc9371d93d31f
Ollie <36059206+olliechap@users.noreply.github.com>
2024-10-17 13:21:47 +1100

    Added Github link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,18 @@ import { FillableButton } from "@/components/fillable-icon-button";
 
 export default function Home() {
   return (
-    <div className="flex flew-wrap items-center justify-center justify-items-center min-h-screen p-8 pb-20 gap-4 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <FillableButton background="bg-yellow-200" fillBackground="bg-pink-300" maxClicks={8} size="sm"/>
-      <FillableButton background="bg-gray-200" fillBackground="bg-yellow-300" maxClicks={8} size="md"/>
-      <FillableButton background="bg-green-200" fillBackground="bg-pink-500" maxClicks={8} size="lg"/>
-      <FillableButton background="bg-blue-200" fillBackground="bg-black" maxClicks={8} size="xl"/>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="flex flew-wrap items-center justify-center justify-items-center p-8 pb-20 gap-4 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <FillableButton background="bg-yellow-200" fillBackground="bg-pink-300" maxClicks={8} size="sm" />
+        <FillableButton background="bg-gray-200" fillBackground="bg-yellow-300" maxClicks={8} size="md" />
+        <FillableButton background="bg-green-200" fillBackground="bg-pink-500" maxClicks={8} size="lg" />
+        <FillableButton background="bg-blue-200" fillBackground="bg-black" maxClicks={8} size="xl" />
+      </div>
+      <div className="flex flew-wrap items-center justify-center justify-items-center p-8 pb-20 gap-4 sm:p-20 font-[family-name:var(--font-geist-sans)] text-black">
+        <a href="https://github.com/olliechap/fillable-button">Github</a>
+        <a href="https://x.com/olliechapman">Twitter</a>
+      </div>
     </div>
+
   );
 }
